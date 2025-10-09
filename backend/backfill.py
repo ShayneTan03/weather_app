ae33c8eb36d8a6e5926b3a4f67304f8a4f6d1659
dioneyongkx <dione8633@gmail.com>
2025-10-10 01:39:15 +0800

    editing backfill code

--- backend/backfill.py
+++ backend/backfill.py
@@ -27,8 +27,8 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 #replace with your own credentials if running locally
 s3_client: boto3.client = boto3.client(
             "s3",
-            aws_access_key_id="AKIAWNNLNZEXWLDBYJGC",
-            aws_secret_access_key="1TzsUEu4EYWUTsQ1vQHeFNNF3Qybzt7IjQiO77VJ",
+            aws_access_key_id="AWS_ACCESS_KEY",
+            aws_secret_access_key="SECRET_ACCESS_KEY",
             # aws_session_token="",
         )
 
