167ee5e9c05974c9f81493fdd05607a4b035f0f3
Javier <javiergoh21@gmail.com>
2025-10-13 02:16:21 +0800

    created v1 working module, to explore corner case of storm does not encompass any weather stations next

---
+++ backend/storm_object_detector/storm_object_detector.py
@@ -0,0 +1,104 @@
+from datetime import datetime 
+import pandas as pd
+import numpy as np
+
+
+
+########################################################################
+# helpers
+def storm_pixel_coordinates(
+        storm_id
+        ,full_storm_grid
+): 
+        # binary mask 
+        storm_mask = (full_storm_grid == storm_id).astype(np.uint8)
+
+        #print(int(storm_mask.sum()))
+
+        # return coordinates for every pixel 
+        ys,xs = np.where(storm_mask)
+
+        # pack into dataframe 
+        coord = list(zip(xs,ys))
+        df = pd.DataFrame({'coord':coord})
+
+        return df
+
+def metric_threshold_calc(
+        input_df
+        ,wind_speed_threshold
+        ,rainfall_threshold
+        ,temperature_threshold
+        ,humidty_threshold
+): 
+        res = False
+        threshold_flags = [False,False,False,False]
+
+        # if single station 
+        if input_df.shape[0] == 1: 
+                if input_df['wind_speed_knots'] >= wind_speed_threshold:
+                        threshold_flags[0] = True
+                if input_df['rainfall_mm'] >= rainfall_threshold:
+                        threshold_flags[1] = True
+                if input_df['temperature_c'] >= temperature_threshold:
+                        threshold_flags[2] = True
+                if input_df['humidity_pct'] >= humidty_threshold:
+                        threshold_flags[3] = True
+
+
+        # if multi station, average all 
+        else:
+                cols_to_avg = ["wind_speed_knots", "rainfall_mm", "temperature_c", "humidity_pct"]
+                mean_df = input_df[cols_to_avg].mean()
+
+                if mean_df['wind_speed_knots'] >= wind_speed_threshold:
+                        threshold_flags[0] = True
+                if mean_df['rainfall_mm'] >= rainfall_threshold:
+                        threshold_flags[1] = True
+                if mean_df['temperature_c'] <= temperature_threshold:
+                        threshold_flags[2] = True
+                if mean_df['humidity_pct'] >= humidty_threshold:
+                        threshold_flags[3] = True
+
+        if sum(threshold_flags) == 4:
+                res = True
+        return res
+########################################################################
+
+########################################################################
+# main function , call this with .apply to vectorise the function on dataframe for faster processing
+def storm_object_checker(
+        storm_id
+        ,possible_storm_grid
+
+        ,weather_data_df
+
+        # threshholds 
+        ,wind_speed_threshold
+        ,rainfall_threshold
+        ,temperature_threshold
+        ,humidty_threshold
+): 
+        """
+        This function is meant to be vectorised on a dataframe, to gauge if the storm meets required thresholds
+
+        inputs: 
+            storm_id : iterable ID from the input dataframe
+            possible_storm_grid : a numppy grid array of labeled components
+            weather_data_df : contains the weather data nearby stations, detected either by 1) storm has a pixel where the weather station is 
+            
+            thresholds: benchmarks to check if the storm is valid 
+
+        outputs
+            a boolean value for every row
+        """
+        curr_storm = storm_pixel_coordinates(storm_id, possible_storm_grid)
+
+        nearby_stations = pd.merge(weather_data_df,curr_storm,how='inner' , on = 'coord')
+
+        res = metric_threshold_calc(nearby_stations,wind_speed_threshold,rainfall_threshold,temperature_threshold,humidty_threshold)
+        
+        return res
+########################################################################
+
+
